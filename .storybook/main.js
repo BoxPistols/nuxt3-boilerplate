72295d6caf5898abf834bc6ff67f8c2458de5f6e
a.ito <10333049+BoxPistols@users.noreply.github.com>
2024-03-24 06:37:25 +0900

    sb iint

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+/** @type { import('@storybook/vue3-webpack5').StorybookConfig } */
+const config = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/vue3-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
